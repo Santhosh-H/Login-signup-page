f08c37baea1edc15b2e23006e6668b1940b41ca6
Santhosh <santhoshhariharan1393@gmail.com>
2023-02-11 21:51:05 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,4 +1,6 @@
 import smtplib 
 server=smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login('santhosh')
+server.login('santhoshpubg13@gmail.com','Santhoshda13')
+server.sendmail('santhoshpubg13@gmail.com','santhoshhariharan1393@gmail.com','Checking for otp')
+print("got")
